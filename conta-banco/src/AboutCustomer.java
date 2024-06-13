e08698cbf5db16d0c72234862b7b21f26fa7af7e
Raphaella Martins <raphaella.karolline@gmail.com>
2024-06-13 14:47:47 -0300

    meu projeto conta banco

---
+++ conta-banco/src/AboutCustomer.java
@@ -0,0 +1,41 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutCustomer {
+    
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
+
+            System.out.println("Olá, seja bem-vindo a página de acesso do seu banco!");
+            System.out.println("Para obter informações sobre o saldo atual, será necessário as seguintes informações: ");
+
+            System.out.println("Digite seu nome:");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome:");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite o número da conta:");
+            Short númeroDaConta = scanner.nextShort();
+
+            System.out.println("Digite o número da agência:");
+            String númeroDaAgência = scanner.next();
+
+            System.out.println("Seu saldo atual é: R$560,50");
+            String saldoAtual = scanner.next();
+
+
+        System.out.println("Nome Completo: " + nome + " " + sobrenome);
+        System.out.println("Número da Agência: " + númeroDaAgência);
+        System.out.println("Número da Conta: " + númeroDaConta);
+        System.out.println(saldoAtual);
+        
+        
+    
+        
+
+    }
+
+
+}
